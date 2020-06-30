9f4ea74c4a5246abc2c134598c695fbe4df4527e
dron5 <dron5@mail.ru>
2020-06-30 15:20:12 +0700

    Deleting all

---
+++ script.js
@@ -0,0 +1,7 @@
+
+    let a = document.querySelector('.press');
+    a.onclick = function () {
+       // alert('хуйняc');
+        document.querySelector('.flex_b').classList.toggle('rel');
+        
+    }
